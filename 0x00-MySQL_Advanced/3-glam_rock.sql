eccdee8c20bbae47fd06a7ab60c7489b50d5daea
progarammingwithjerry <eneyellowj@gmail.com>
2024-11-07 17:44:55 +0100

    3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,10 +1,11 @@
 -- Select bands with Glam rock as the main style, calculating lifespan and ordering by longevity
+
 SELECT 
     band_name, 
-    (IFNULL(split, 2022) - formed) AS lifespan
+    (IFNULL(split, 2023) - formed) AS lifespan
 FROM 
     metal_bands
 WHERE 
-    style = 'Glam rock'
+    FIND_IN_SET('Glam rock', IFNULL(style, '')) > 0
 ORDER BY 
     lifespan DESC;
