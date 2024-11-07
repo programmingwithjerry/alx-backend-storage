8a23cc1a37f86afb1661fb99396a96ccfd7b4fe9
progarammingwithjerry <eneyellowj@gmail.com>
2024-11-07 18:08:39 +0100

    10-div.sql

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -3,7 +3,7 @@
 DELIMITER $$
 
 CREATE FUNCTION SafeDiv(a INT, b INT) 
-RETURNS INT
+RETURNS DECIMAL(20, 10)  -- Updated return type to DECIMAL for higher precision
 DETERMINISTIC
 BEGIN
     -- Check if the denominator is 0
@@ -11,8 +11,8 @@ BEGIN
         -- Return 0 if the second number is 0
         RETURN 0;
     ELSE
-        -- Perform the division and return the result
-        RETURN a / b;
+        -- Perform the division with casting to DECIMAL for precise result
+        RETURN CAST(a AS DECIMAL(20, 10)) / CAST(b AS DECIMAL(20, 10));
     END IF;
 END $$
 
