42514c5ff395e82fb6c6016b62fe643c3f8453dd
progarammingwithjerry <eneyellowj@gmail.com>
2024-11-10 23:30:29 +0100

    8-all.py

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+""" Lists all documents in a collection """
+
 def list_all(mongo_collection):
     """
     Lists all documents in a MongoDB collection.
@@ -9,4 +12,5 @@ def list_all(mongo_collection):
     documents = mongo_collection.find()
     
     # Convert the cursor to a list and return it
-    return [doc for doc in documents]
+    # return [doc for doc in documents]
+    return list(documents)
